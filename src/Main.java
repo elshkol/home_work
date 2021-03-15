acaed2b23de649276b2190eac887d7d46c9a5e61
Elena Shkolnaya <elshkol@mail.ru>
2021-03-15 23:07:44 +0400

    третья задача

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Введите любое целое число: ");
+        System.out.println("Введите количество элементов массива: ");
         Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        if (number >= 0) ;
-        {
-            System.out.println(Integer.toBinaryString(number));
+        int numberMas = scan.nextInt();
+        if (numberMas > 0) {
+            int[] mas = new int[numberMas];
+            int count = 0;
+            for (int i = 1; i <= numberMas; i++){
+                System.out.println("Введите элемент массива № " + i);
+                mas[i - 1] = scan.nextInt();
+            }
+            String result = "";
+            for (int i = 0; i < numberMas; i++) {
+                result = result + Integer.toString(mas[i]);
+            }
+            System.out.println("Результат (строка):" + result);
+            System.out.println("Результат (число):" + Integer.valueOf(result));
         }
     }
 }
