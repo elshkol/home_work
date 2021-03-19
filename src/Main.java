431f76fb8d3b2d96c9167fd22930d097186d142f
Elena Shkolnaya <elshkol@mail.ru>
2021-03-19 21:58:45 +0400

    Пятая задача

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,36 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        Scanner ma = new Scanner(System.in);
-        System.out.println("Введите количество элементов:");
-        int num = ma.nextInt();
-        int a[] = new int[num];
-        System.out.println("Введите элементы:");
-        for (int i = 0; i < num; i++) {
-            a[i] = ma.nextInt();
-        }
-        System.out.println("Элементы массива в обратном порядке:");
-        for (int i = a.length - 1; i >= 0; i--) {
-            System.out.println(a[i]);
+    public static void main(String args[]) {
+        System.out.println("Введите количество элементов массива: ");
+        Scanner scan = new Scanner(System.in);
+        int numberMas = scan.nextInt();
+        if (numberMas > 0) {
+            String[] mas = new String[numberMas];
+            // int count = 0;
+            for (int i = 1; i <= numberMas; i++) {
+                System.out.println("Введите элемент массива № " + i);
+                mas[i - 1] = scan.next();
+            }
+            char bykva;
+            for (int i = 0; i < numberMas; i++) {
+                for (int j = 0; j <= mas[i].length() - 1; j++){
+                    bykva = mas[i].charAt(j);
+                    if (Character.isLowerCase(bykva)) {
+                        System.out.print(Character.toUpperCase(bykva));
+                    }
+                    else { // иначе
+                        System.out.print(Character.toLowerCase(bykva));
+                    }
+                }
+            }
         }
     }
 }
+
+
+
+
+
 
 
 
